d9df8c8bf4c8119df6dc40b8219060ff870a682c
Mrigendra <mrigendra21@gmail.com>
2020-03-15 22:15:21 +0530

    Done!

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -120,4 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'   # this is just a path name in URL. How it will be printed. You can name it like "mypics"
