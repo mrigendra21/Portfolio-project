0a1179b724bac8a987e75ebd4ce1ad0edcc1705a
Mrigendra <mrigendra21@gmail.com>
2020-03-15 21:03:36 +0530

    Done!

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')  # Images with the same name will be renamed differently by Django. User need not to worry!
+    summary = models.CharField(max_length=200)      # This sets the maximum length of character for each Job item.
